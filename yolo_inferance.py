0227e71badc3167e0c94c828fa36ea2e7e7ad362
Harshithvarma007 <112501476+Harshithvarma007@users.noreply.github.com>
2024-07-05 19:18:31 +0530

    ball tracking model traininng completed

--- yolo_inferance.py
+++ yolo_inferance.py
@@ -1,6 +1,10 @@
 from ultralytics import YOLO
 
-model=YOLO('yolov10x')
+model=YOLO('Models\yolov5_best.pt')
 
-model.predict('input_videos\input_video.mp4',save= True)
+result=model.predict('input_videos\input_video.mp4',save= True)
 
+print(result)
+print("boxes:")
+for box in result[0].boxes:
+    print(box)
