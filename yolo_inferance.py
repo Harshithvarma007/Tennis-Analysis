3ba419b4a981790eae679948517d63b7a5c856d2
Harshithvarma007 <112501476+Harshithvarma007@users.noreply.github.com>
2024-07-05 19:19:39 +0530

    prediction

--- yolo_inferance.py
+++ yolo_inferance.py
@@ -4,7 +4,7 @@ model=YOLO('Models\yolov5_best.pt')
 
 result=model.predict('input_videos\input_video.mp4',save= True)
 
-print(result)
-print("boxes:")
-for box in result[0].boxes:
-    print(box)
+# print(result)
+# print("boxes:")
+# for box in result[0].boxes:
+#     print(box)
